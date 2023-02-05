3ed0dfe3847ed5a2aef58f40e843d5e8c8842529
Gulshan Sharma <102547674+GulshanSharma0@users.noreply.github.com>
2023-02-06 02:14:06 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,95 @@
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+// var name = 'Kashyap';
+// var sentence = `This is Ankit ${name}`;
+// console.log(sentence);
+// document.getElementById('thisis').innnerHTML = '<h4>New line is inserted</h4>'
+// function thisName(name){
+// var his = 'WHAT ISTHIS';
+// var hus = 'This is not good for me ${his}';
+// console.log(hus);
+// let age = 2;
+// if (age > 20) {
+//     console.log("You can shot the bullet");
+// }
+// else {
+//     console.log("You cant 'shot' the bullet on me in this era.");
+// }
+//     console.log("Hello " + name);
+// }
+// var a = null;
+// var b = 8;
+// console.log(a);
+// a = 10;
+// b = a;
+// console.log(a);
+// console.log("First line\n\\\tSecond line\nThird line");
+// var fruit = "";
+// var vegetables = "Tomato";
+// fruit = vegetables[5];
+// console.log(vegetables.charAt(4));
+// thisName("Ankit");
+
+// Array starts here 
+
+// var Marvel = [2, 5, 9, 5, 6, 8, 2, 4, 6, 70, 80, 90];
+// console.log(phase);
+// Marvel.push(50, 60, 70, 1, 2);
+// var phase = Marvel[16];
+// console.log(phase);
+
+// function test(number)
+// {
+//    while(number < 5) {
+//       number++;
+//    }
+//    return number;
+// }
+// alert(test(2));
+
+// var sum = 0;
+// sum = returnSum();
+// function returnSum() {
+//     sum += 63;
+//     return sum;
+// }
+// console.log(sum);
+
+// function nextInLine(arr, item) {
+    
+    
+//     return
+// }
+
+// function Z(A){
+//     for(var i =0; i<=M.length; i++);{
+//         if(i%2==0){
+//             var N = M.splice(i,1);
+//             return M;
+//         }
+//     }
+// }
+
+// var M = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+// console.log("Odd Elements :" + JSON.stringify(Z(M)));
+// console.log(M.leng
+
+var a = "Ankit";
+var b = " Kumar";
+console.log(a+b);
+const int = 20;
+console.log(int);
